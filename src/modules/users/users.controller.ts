0fe5c74f7289a64a163579761f19e5eff5a54e8b
Sofia Dev <dev@sofia.com>
2026-03-18 07:44:08 -0600

    refactor(auth): remove old RolesGuard system, use SystemAdminGuard for /api/users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,8 +6,7 @@ import {
   ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { JwtAuthGuard } from '@modules/auth/guards/jwt-auth.guard';
-import { RolesGuard } from '@common/guards/roles.guard';
-import { Roles } from '@common/decorators/roles.decorator';
+import { SystemAdminGuard } from '@common/guards/system-admin.guard';
 import { UserRepository } from './repositories/user.repository';
 import { UserResponseDto } from './dto/user-response.dto';
 
@@ -17,8 +16,7 @@ export class UsersController {
   constructor(private readonly userRepository: UserRepository) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @UseGuards(JwtAuthGuard, SystemAdminGuard)
   async findAll(): Promise<UserResponseDto[]> {
     const users = await this.userRepository.findAll();
     return users.map((user) => new UserResponseDto(user));
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,12 +3,11 @@ import { UserRepository } from '@modules/users/repositories/user.repository';
 import { UsersController } from '@modules/users/users.controller';
 import { InternalUsersController } from './internal-users.controller';
 import { AuthModule } from '@modules/auth/auth.module';
-import { RolesGuard } from '@common/guards/roles.guard';
 
 @Module({
   imports: [forwardRef(() => AuthModule)],
   controllers: [UsersController, InternalUsersController],
-  providers: [UserRepository, RolesGuard],
+  providers: [UserRepository],
   exports: [UserRepository],
 })
 export class UsersModule {}
